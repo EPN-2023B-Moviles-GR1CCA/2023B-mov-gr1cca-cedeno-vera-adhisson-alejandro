eb536f6c8cc0dc1bd18eaf2887b8c0ee89ff7b69
Adhisson Cedeño <adhisson89@hotmail.com>
2024-01-03 16:32:25 -0500

    xd
    no se que pasa

---
+++ 02-android/app/src/main/java/com/example/gr1aacv2023b/ACicloVida.kt
@@ -0,0 +1,58 @@
+package com.example.gr1aacv2023b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+
+class ACicloVida : AppCompatActivity() {
+
+    var textoGLobal = ""
+
+    fun mostrarSnackbar(texto: String){
+        textoGLobal +=  "texto"
+        Snackbar
+            .make(
+                findViewById(R.id.cl_ciclo_vida), //view
+                textoGLobal, //texto
+                Snackbar.LENGTH_INDEFINITE //tiempo
+            )
+            .show()
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+        mostrarSnackbar("OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        mostrarSnackbar("onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("onDestroy")
+    }
+
+
+}
---
+++ 02-android/app/src/main/java/com/example/gr1aacv2023b/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gr1aacv2023b
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
