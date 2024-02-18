44f455db049bce3d592e0d0b83175c37a2b8dc25
Adhisson Cedeño <adhisson89@hotmail.com>
2024-02-18 18:57:17 -0500

    icono y colores

--- 07-deber03-reciclerview/app/src/main/java/com/example/galeria/MainActivity.kt
+++ 07-deber03-reciclerview/app/src/main/java/com/example/galeria/MainActivity.kt
@@ -4,15 +4,21 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CursorAdapter
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.loader.content.CursorLoader
@@ -31,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         imageRecycler = findViewById(R.id.image_recycler)
         progressBar = findViewById(R.id.recycler_progress)
 
-        imageRecycler?.layoutManager = GridLayoutManager(this, 4) // tamaño de las imágenes
+        imageRecycler?.layoutManager = GridLayoutManager(this, 3) // tamaño de las imágenes
         imageRecycler?.setHasFixedSize(true)
 
         /*
@@ -62,6 +68,40 @@ class MainActivity : AppCompatActivity() {
             // Si se conceden los permisos, carga las imágenes
             loadImages()
         }
+
+// Centrar el título de la ActionBar y mostrar el icono de la aplicación
+        val actionBar: ActionBar? = supportActionBar
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.HORIZONTAL
+        layout.gravity = Gravity.CENTER
+
+        val imageView = ImageView(this)
+        val drawable: Drawable? = ContextCompat.getDrawable(this, R.mipmap.ic_launcher)
+        imageView.setImageDrawable(drawable)
+        imageView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        val textView = TextView(this)
+        textView.text = actionBar?.title
+        textView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        layout.addView(imageView)
+        layout.addView(textView)
+
+        val layoutParams = Toolbar.LayoutParams(
+            Toolbar.LayoutParams.WRAP_CONTENT,
+            Toolbar.LayoutParams.WRAP_CONTENT,
+            Gravity.CENTER
+        )
+
+        actionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        actionBar?.customView = layout
+        actionBar?.customView?.layoutParams = layoutParams
 
 
     } // onCreate
